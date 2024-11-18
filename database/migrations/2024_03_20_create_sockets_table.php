d1f53382add7cf7ba80fbe5e97ebca5bf32bceff
imadama <99074774@mydavinci.nl>
2024-11-18 12:03:35 +0100

    Je kan nu de socket aan en uit doen

--- database/migrations/2024_03_20_create_sockets_table.php
+++ database/migrations/2024_03_20_create_sockets_table.php
@@ -4,30 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSocketsTable extends Migration
+return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('sockets', function (Blueprint $table) {
             $table->id();
+            $table->string('tasmota_id')->unique();
             $table->string('name');
-            $table->string('ip_address');
+            $table->string('status')->default('offline');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
     public function down()
     {
         Schema::dropIfExists('sockets');
     }
-}
+}; 
